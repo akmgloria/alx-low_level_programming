b1f1c2630cdbe1852f6ef16583b54b902839cdd5
akmgloria <akmgloria@gmail.com>
2022-09-13 17:17:25 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * main - Lanzador.
+ *
+ * Return: Always 0.
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+		char z = 'z';
+
+
+
+			for (z = 'z'; z >= 'a'; z--)
+
+					{
+
+								putchar(z);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+}
